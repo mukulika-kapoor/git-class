dd0f8960190c0214a2c463e184507c726ea8b7a6
unknown <mukulika.kapoor@gmail.com>
2021-01-10 20:37:29 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var bgimg, platform;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    bgimg = loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(200, 300, 400,250);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -38,14 +39,15 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background(bgimg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
+    ground.display();
+    platform.display();
+
     box1.display();
     box2.display();
-    ground.display();
+    
     pig1.display();
     log1.display();
 
